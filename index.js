d3e2e94370bedcdc6ac2435839aad10023a9d405
Rishikmanche <rishikmanche@gmail.com>
2026-07-26 22:26:54 +0530

    Stage 5: Swagger UI

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const openapiDoc = require("./openapi.json");
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(openapiDoc));
 
 let tasks = [
   { id: 1, title: "Learn Express", done: false },
